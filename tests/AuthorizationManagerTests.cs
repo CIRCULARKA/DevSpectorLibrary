b8ff5db996214168bb0c5ca994586dea72a42d22
CIRCULARKA <donkardon@list.ru>
2022-02-20 16:56:30 +0500

    Adapt tests

--- tests/AuthorizationManagerTests.cs
+++ tests/AuthorizationManagerTests.cs
@@ -1,8 +1,8 @@
 using System;
 using System.Threading.Tasks;
 using Xunit;
+using DevSpector.SDK;
 using DevSpector.SDK.Authorization;
-using DevSpector.SDK.Models;
 
 namespace DevSpector.Tests.Common.SDK.Authorization
 {
@@ -11,32 +11,10 @@ namespace DevSpector.Tests.Common.SDK.Authorization
         private readonly string _hostname = "dev-devspector.herokuapp.com";
 
         [Fact]
-        public void IsHostBuiltProperly()
-        {
-            // Arrange
-            var expectedHost1 = "testhost";
-            var expectedHost2 = "localhost";
-
-            var expectedPort1 = 5000;
-            var expectedPort2 = 5043;
-
-            // Act
-            var manager1 = new AuthorizationManager(expectedHost1, expectedPort1);
-            var manager2 = new AuthorizationManager(expectedPort2);
-
-            // Assert
-            Assert.Equal(expectedHost1, manager1.Host.Host);
-            Assert.Equal(expectedPort1, manager1.Host.Port);
-
-            Assert.Equal(expectedHost2, manager2.Host.Host);
-            Assert.Equal(expectedPort2, manager2.Host.Port);
-        }
-
-        [Fact]
         public async Task CanGetUser()
         {
             // Arrange
-            var manager = new AuthorizationManager(_hostname);
+            var manager = new AuthorizationManager(new HostBuilder(_hostname, scheme: "https"));
 
             var login = "root";
             var password = "123Abc!";
@@ -54,7 +32,7 @@ namespace DevSpector.Tests.Common.SDK.Authorization
         public async Task CantGetUser()
         {
             // Arrange
-            var manager = new AuthorizationManager(_hostname);
+            var manager = new AuthorizationManager(new HostBuilder(_hostname, scheme: "https"));
 
             var expectedLogin = "noname";
             var password = "no";
