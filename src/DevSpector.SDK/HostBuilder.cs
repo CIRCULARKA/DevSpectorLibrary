4bb95ef73c5b14f7ab6fc139f55264a875710908
CIRCULARKA <donkardon@list.ru>
2022-02-20 16:35:02 +0500

    Add host builder builder to build uri's

---
+++ src/DevSpector.SDK/HostBuilder.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+
+namespace DevSpector.SDK
+{
+    public class HostBuilder : IHostBuilder
+    {
+        private string _hostname;
+
+        private int _port;
+
+        private string _scheme;
+
+        public HostBuilder(string hostname = "localhost", int port = 443, string scheme = "http")
+        {
+            _hostname = hostname;
+            _port = port;
+            _scheme = scheme;
+
+            Host = CreateHost();
+        }
+
+        public Uri Host { get; }
+
+        private Uri CreateHost()
+        {
+            var buidler = new UriBuilder();
+
+            buidler.Host = _hostname;
+            buidler.Port = _port;
+            buidler.Scheme = _scheme;
+
+            return buidler.Uri;
+        }
+
+        public Uri BuildTargetEndpoint(string path, Dictionary<string, string> parameters)
+        {
+            var builder = new UriBuilder();
+
+            builder.Host = Host.Host;
+            builder.Port = Host.Port;
+            builder.Path = path;
+
+            // Build query upon dictionary parameters
+            // by using key as parameter name and value as parameter value
+            // Also append '&' on the end of each parameter if it is not last
+            var query = new StringBuilder();
+            for (int i = 0; i < parameters.Count; i++)
+            {
+                var pair = parameters.ElementAt(i);
+                query.Append($"{pair.Key}={pair.Key}");
+                if (i < parameters.Count - 1)
+                    query.Append("&");
+            }
+
+            builder.Scheme = Host.Scheme;
+
+            return builder.Uri;
+        }
+    }
+}
