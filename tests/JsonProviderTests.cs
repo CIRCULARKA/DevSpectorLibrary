ac7df7796fb3312de2e7a026ff9a0a4b2dbf7c9e
CIRCULARKA <donkardon@list.ru>
2022-02-20 16:50:36 +0500

    Remove one useless test from JsonProviderTests

--- tests/JsonProviderTests.cs
+++ tests/JsonProviderTests.cs
@@ -12,7 +12,7 @@ namespace DevSpector.SDK.Tests
         public async void ThrowsWrongAPI()
         {
             // Arrange
-            var provider = new JsonProvider(_host);
+            var provider = new JsonProvider(new HostBuilder(_host, scheme: "https"));
 
             var actions = new Func<Task<string>>[] {
                 async () => await provider.GetDevicesAsync("wrongToken"),
@@ -22,7 +22,6 @@ namespace DevSpector.SDK.Tests
                 async () => await provider.GetUsersAsync("wrongToken")
             };
 
-
             // Assert
             await Assert.ThrowsAsync<ArgumentException>(
                 async () => {
@@ -30,39 +29,6 @@ namespace DevSpector.SDK.Tests
                         await action.Invoke();
                 }
             );
-        }
-
-        [Fact]
-        public void DoesUriBuiltProperly()
-        {
-            // Arrange
-            var expectedHost1 = "testHost1";
-            var expectedPort1 = 1234;
-
-            var expectedHost2 = "testHost2";
-            int expectedPort2 = 443;
-
-            var expectedHost3 = "localhost";
-            var expectedPort3 = 5040;
-
-            // Act
-            var provider1 = new JsonProvider(expectedHost1, expectedPort1);
-            var provider2 = new JsonProvider(expectedHost2);
-            var provider3 = new JsonProvider(expectedPort3);
-
-            var uri1 = provider1.Host;
-            var uri2 = provider2.Host;
-            var uri3 = provider3.Host;
-
-            // Assert
-            Assert.Equal(expectedHost1, uri1.Host, ignoreCase: true);
-            Assert.Equal(expectedPort1, uri1.Port);
-
-            Assert.Equal(expectedHost2, uri2.Host, ignoreCase: true);
-            Assert.Equal(expectedPort2, uri2.Port);
-
-            Assert.Equal(expectedHost3, uri3.Host, ignoreCase: true);
-            Assert.Equal(expectedPort3, uri3.Port);
         }
     }
 }
