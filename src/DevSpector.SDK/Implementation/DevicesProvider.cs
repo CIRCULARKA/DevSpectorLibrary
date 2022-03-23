d905b4ad191de530ff8662c2979b96260e795bad
CIRCULARKA <donkardon@list.ru>
2022-03-23 11:35:51 +0500

    Implement provider according to new IRawDataProvider

--- src/DevSpector.SDK/Implementation/DevicesProvider.cs
+++ src/DevSpector.SDK/Implementation/DevicesProvider.cs
@@ -1,4 +1,4 @@
-using System.Text.Json;
+using System;
 using System.Threading.Tasks;
 using System.Collections.Generic;
 using DevSpector.SDK.Models;
@@ -14,12 +14,16 @@ namespace DevSpector.SDK
 			_provider = provider;
 		}
 
-		public async Task<IEnumerable<Appliance>> GetDevicesAsync(string accessToken) =>
-			JsonSerializer.Deserialize<List<Appliance>>(
-				await _provider.GetDevicesAsync(accessToken),
-				new JsonSerializerOptions() {
-					PropertyNameCaseInsensitive = true
-				}
+		public async Task<List<Appliance>> GetDevicesAsync(string accessToken)
+		{
+			var response = await _provider.GetDataFromServer("api/devices", accessToken);
+
+			if (!response.IsSucceed)
+				throw new InvalidOperationException($"Failed to load devices from server: error {response.ResponseStatusCode}");
+
+			return _provider.Deserialize<List<Appliance>>(
+				response.ResponseContent
 			);
+		}
 	}
 }
