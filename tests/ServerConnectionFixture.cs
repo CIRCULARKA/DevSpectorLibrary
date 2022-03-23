476395dfc5164e6b78ad7c4923d13e3017ab00fd
CIRCULARKA <donkardon@list.ru>
2022-03-23 18:00:36 +0500

    Add new method to fixture

--- tests/ServerConnectionFixture.cs
+++ tests/ServerConnectionFixture.cs
@@ -1,21 +1,39 @@
 using System.Text.Json;
 using System.Net.Http;
 using System.Threading.Tasks;
+using DevSpector.SDK.Models;
 
 namespace DevSpector.Tests
 {
     public class ServerConnectionFixture
     {
+        private readonly HttpClient _client;
+
+        public ServerConnectionFixture()
+        {
+            _client = new HttpClient();
+        }
+
         public async Task<T> GetFromServerAsync<T>(string address)
         {
-            var client = new HttpClient();
-            var response = await client.GetAsync(address);
+            var response = await _client.GetAsync(address);
             var responseContent = await response.Content.ReadAsStringAsync();
 
-            return JsonSerializer.Deserialize<T>(
-                responseContent,
+            return DeserializeJson<T>(responseContent);
+        }
+
+        public async Task<User> GetAuthorizedUser()
+        {
+            var response = await _client.GetAsync("https://dev-devspector.herokuapp.com/api/users/authorize?login=root&password=123Abc!");
+            var responseContent = await response.Content.ReadAsStringAsync();
+
+            return DeserializeJson<User>(responseContent);
+        }
+
+        private T DeserializeJson<T>(string json) =>
+            JsonSerializer.Deserialize<T>(
+                json,
                 new JsonSerializerOptions { PropertyNameCaseInsensitive = true }
             );
-        }
     }
 }
