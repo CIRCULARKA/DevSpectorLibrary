bf84e6cd24a3ac5af8963856a8e93152b18a0b3f
CIRCULARKA <donkardon@list.ru>
2022-03-23 11:30:13 +0500

    Update IRawDataProvider

--- src/DevSpector.SDK/Interfaces/IRawDataProvider.cs
+++ src/DevSpector.SDK/Interfaces/IRawDataProvider.cs
@@ -1,7 +1,12 @@
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using DevSpector.SDK.Models;
+
 namespace DevSpector.SDK
 {
 	public interface IRawDataProvider
 	{
         TOut Deserialize<TOut>(string json);
+        Task<ServerResponse> GetDataFromServer(string path, string accessToken = null, Dictionary<string, string> parameters = null);
 	}
 }
