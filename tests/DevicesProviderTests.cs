0ad43ac052d44356c69a9f81983874bec448f019
CIRCULARKA <donkardon@list.ru>
2022-03-23 18:15:18 +0500

    Fix test for DevicesProvider

--- tests/DevicesProviderTests.cs
+++ tests/DevicesProviderTests.cs
@@ -1,109 +1,63 @@
-using System;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Collections.Generic;
-using Moq;
 using Xunit;
 using DevSpector.SDK;
 using DevSpector.SDK.Models;
 
-namespace DevSpector.Tests.Server.SDK
+namespace DevSpector.Tests.SDK
 {
+	[Collection(nameof(FixtureCollection))]
 	public class DevicesProviderTests
 	{
-		private readonly IRawDataProvider _mockDataProvider;
+		private readonly ServerConnectionFixture _connectionFixture;
 
-		private readonly Guid[] _mockDevicesGuids;
+		private readonly IRawDataProvider _rawDataProvider;
 
-		public DevicesProviderTests()
+		private readonly IDevicesProvider _devicesProvider;
+
+		public DevicesProviderTests(ServerConnectionFixture conFix)
 		{
-			_mockDevicesGuids = new Guid[] {
-				new Guid("16036105-5111-4420-8b26-a18deaeb8f9b"),
-				new Guid("ed8c1437-07fd-4ce8-beb2-aba831d05e31"),
-			};
+			_connectionFixture = conFix;
 
-			var moq = new Mock<IRawDataProvider>();
-			moq.Setup(provider => provider.GetDevicesAsync(Guid.Empty.ToString())).
-				Returns(
-					Task.FromResult<string>(
-						@"[
-							{
-								""id"": ""16036105-5111-4420-8b26-a18deaeb8f9b"",
-								""inventoryNumber"": ""inv1"",
-								""networkName"": ""net1"",
-								""type"": ""type1"",
-								""housing"": ""h1"",
-								""cabinet"": ""cab1"",
-								""ipAddresses"": [
-									""1.1.1.1"",
-									""2.2.2.2""
-								]
-							},
-							{
-								""id"": ""ed8c1437-07fd-4ce8-beb2-aba831d05e31"",
-								""inventoryNumber"": ""inv2"",
-								""networkName"": ""net2"",
-								""type"": ""type2"",
-								""housing"": ""h2"",
-								""cabinet"": ""cab2"",
-								""ipAddresses"": [
-									""3.3.3.3"",
-									""4.4.4.4""
-								]
-							}
-						]"
-					)
-				);
+			_rawDataProvider = new JsonProvider(new HostBuilder("dev-devspector.herokuapp.com", scheme: "https"));
 
-			_mockDataProvider = moq.Object;
+			_devicesProvider = new DevicesProvider(_rawDataProvider);
 		}
 
 		[Fact]
-		public async void AreDevicesDeserializedProperly()
+		public async void CanGetDevices()
 		{
 			// Arrange
-			var provider = new DevicesProvider(_mockDataProvider);
+			User user = await _connectionFixture.GetAuthorizedUser();
 
-			var expected = new List<Appliance>
-			{
-				new Appliance(_mockDevicesGuids[0], "inv1", "type1", "net1",
-					"h1", "cab1",
-					new List<string> {
-						"1.1.1.1",
-						"2.2.2.2",
-					},
-					null
-				),
-				new Appliance(_mockDevicesGuids[1], "inv2", "type2", "net2",
-					"h2", "cab2",
-					new List<string> {
-						"3.3.3.3",
-						"4.4.4.4",
-					},
-					null
-				)
-			};
+			List<Appliance> expected = await _connectionFixture.GetFromServerAsync<List<Appliance>>(
+				$"https://dev-devspector.herokuapp.com/api/devices?api={user.AccessToken}"
+			);
 
 			// Act
-			var actual = (await provider.GetDevicesAsync(Guid.Empty.ToString())).ToList();
+			var actual = await _devicesProvider.GetDevicesAsync(user.AccessToken);
 
 			// Assert
-			Assert.Equal(expected.Count(), actual.Count());
+			Assert.Equal(expected.Count, actual.Count);
 
-			for (int i = 0; i < expected.Count(); i++)
+			for (int i = 0; i < expected.Count; i++)
 			{
-				var expectedIPList = expected[i].IPAddresses.ToList();
-				var actualIPList = actual[i].IPAddresses.ToList();
-				Assert.Equal(expectedIPList.Count, actualIPList.Count);
 				Assert.Equal(expected[i].ID, actual[i].ID);
 				Assert.Equal(expected[i].InventoryNumber, actual[i].InventoryNumber);
 				Assert.Equal(expected[i].NetworkName, actual[i].NetworkName);
 				Assert.Equal(expected[i].Housing, actual[i].Housing);
 				Assert.Equal(expected[i].Cabinet, actual[i].Cabinet);
-				for (int j = 0; j < expected[i].IPAddresses.Count(); j++)
-				{
-					Assert.Equal(expectedIPList[i], actualIPList[i]);
-				}
+
+				var expectedIPList = expected[i].IPAddresses;
+				var actualIPList = actual[i].IPAddresses;
+				Assert.Equal(expectedIPList.Count, actualIPList.Count);
+				for (int j = 0; j < expectedIPList.Count; j++)
+					Assert.Equal(expectedIPList[j], actualIPList[j]);
+
+				var expectedSoftware = expected[i].Software;
+				var actualSoftware = actual[i].Software;
+				Assert.Equal(expectedSoftware.Count, actualSoftware.Count);
+				for (int j = 0; j < expectedSoftware.Count; j++)
+					Assert.Equal(expectedSoftware[j], actualSoftware[j]);
 			}
 		}
 	}
