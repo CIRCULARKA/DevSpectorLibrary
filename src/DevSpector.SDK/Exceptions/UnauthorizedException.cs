9f279974df900469e48fd885a1e2c1b95e8f6328
CIRCULARKA <donkardon@list.ru>
2022-03-24 11:00:32 +0500

    Add some documentation to class

--- src/DevSpector.SDK/Exceptions/UnauthorizedException.cs
+++ src/DevSpector.SDK/Exceptions/UnauthorizedException.cs
@@ -2,6 +2,10 @@ using System;
 
 namespace DevSpector.SDK.Exceptions
 {
+    /// <summary>
+    /// This exception must be thrown if server denies access to its resources
+    /// because issuer has no rights to get access
+    /// </summary>
     public class UnauthorizedException : Exception
     {
         public UnauthorizedException(string message) : base(message) { }
