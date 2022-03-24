3d3b011977d4bffbb43b3bcd54603633c447f576
CIRCULARKA <donkardon@list.ru>
2022-03-24 11:02:58 +0500

    Fix typo

--- src/DevSpector.SDK/Exceptions/UnauthorizedException.cs
+++ src/DevSpector.SDK/Exceptions/UnauthorizedException.cs
@@ -4,7 +4,7 @@ namespace DevSpector.SDK.Exceptions
 {
     /// <summary>
     /// This exception must be thrown if server denies access to its resources
-    /// because issuer has no rights to get access
+    /// because issuer has no rights to get the access
     /// </summary>
     public class UnauthorizedException : Exception
     {
