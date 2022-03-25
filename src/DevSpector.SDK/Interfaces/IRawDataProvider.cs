7a3d19c648ee5b0dd5940766425e268d125375d4
CIRCULARKA <donkardon@list.ru>
2022-03-25 14:17:11 +0500

    Inject access token through constructor

--- src/DevSpector.SDK/Interfaces/IRawDataProvider.cs
+++ src/DevSpector.SDK/Interfaces/IRawDataProvider.cs
@@ -10,8 +10,8 @@ namespace DevSpector.SDK
 
         string Serialize<T>(T obj);
 
-        Task<ServerResponse> GetDataFromServerAsync(string path, string accessToken = null, Dictionary<string, string> parameters = null);
+        Task<ServerResponse> GetDataFromServerAsync(string path, Dictionary<string, string> parameters = null);
 
-		Task<ServerResponse> PostDataToServerAsync<T>(string path, T obj, string accessToken = null, Dictionary<string, string> parameters = null);
+		Task<ServerResponse> PostDataToServerAsync<T>(string path, T obj, Dictionary<string, string> parameters = null);
 	}
 }
