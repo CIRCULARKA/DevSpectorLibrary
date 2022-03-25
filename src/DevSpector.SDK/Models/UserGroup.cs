92a95a5697aea5a780fd7130390f755b2ab173eb
CIRCULARKA <donkardon@list.ru>
2022-03-25 10:11:53 +0500

    Add public constructor to Model

--- src/DevSpector.SDK/Models/UserGroup.cs
+++ src/DevSpector.SDK/Models/UserGroup.cs
@@ -2,6 +2,12 @@ namespace DevSpector.SDK.Models
 {
     public class UserGroup
     {
+        public UserGroup(string id, string name)
+        {
+            ID = id;
+            Name = name;
+        }
+
         public string ID { get; }
 
         public string Name { get; }
