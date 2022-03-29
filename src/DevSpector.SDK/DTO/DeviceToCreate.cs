d63ed0f59b4efcebb3b8b11ee5f3a4b79fbdbe53
CIRCULARKA <donkardon@list.ru>
2022-03-29 10:23:42 +0500

    Ignore properties if they are null

--- src/DevSpector.SDK/DTO/DeviceToCreate.cs
+++ src/DevSpector.SDK/DTO/DeviceToCreate.cs
@@ -1,13 +1,19 @@
+using System.Text.Json.Serialization;
+
 namespace DevSpector.SDK.DTO
 {
     public class DeviceToCreate
     {
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         public string InventoryNumber { get; set; }
 
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         public string NetworkName { get; set; }
 
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         public string TypeID { get; set; }
 
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         public string ModelName { get; set; }
     }
 }
