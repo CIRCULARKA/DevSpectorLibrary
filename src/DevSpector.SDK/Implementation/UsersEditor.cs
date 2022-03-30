d4d45067014f0a49371eca3fb2bbb5ebbccbe9cf
CIRCULARKA <donkardon@list.ru>
2022-03-30 11:27:08 +0500

    Define implementation of CreateUser

---
+++ src/DevSpector.SDK/Implementation/UsersEditor.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Threading.Tasks;
+using DevSpector.SDK.DTO;
+
+namespace DevSpector.SDK.Editors
+{
+    public class UsersEditor : IUsersEditor
+    {
+        public Task CreateUser(UserToCreate userInfo)
+        {
+            throw new NotImplementedException("Method not tested yet");
+        }
+    }
+}
